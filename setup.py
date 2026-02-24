3f68abf28623d0461c56dd9c1aa263e6419c4259
Shivangani <Shivanganii001@gmail.com>
2026-02-24 14:58:20 +0530

    Fix setup.py dependencies for Render

--- setup.py
+++ setup.py
@@ -6,3 +6,16 @@ setup(
     author_email="<Shivanganii001@gmail.com>",
     packages=find_packages(),
     install_requires=[])
+
+install_requires=[
+    "flask==3.1.1",
+    "gunicorn==21.2.0",
+    "langchain==0.3.26",
+    "langchain-community==0.3.26",
+    "langchain-openai==0.3.24",
+    "langchain-pinecone==0.1.3",
+    "langchain-nvidia-ai-endpoints==0.3.2",
+    "sentence-transformers==4.1.0",
+    "pypdf==5.6.1",
+    "python-dotenv==1.1.0"
+]
